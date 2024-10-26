7e57fae4aee11ecf8bacdee209a3bf3913128713
djLee0831 <dreamsoft00@knou.ac.kr>
2024-10-26 21:01:52 +0900

    'pybo' 대신 __name__ 으로 변경

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
 import logging
-logger = logging.getLogger('pybo')
+logger = logging.getLogger(__name__)
 
 
 def index(request):
